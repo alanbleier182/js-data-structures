5f283d91070c75d0732adeb162ab78bd3e0c18b2
alanbleier182 <alanbleier182@gmail.com>
2021-10-14 14:02:20 -0500

    Add a .findNodeRecursively method

--- LinkedList.js
+++ LinkedList.js
@@ -34,6 +34,16 @@ class LinkedList {
     this.head = removedHead.getNextNode();
     return removedHead.data;
   }
+  
+  findNodeRecursively(data, currentNode = this.head) {
+    if (currentNode === null) {
+      return null;
+    } else if (currentNode.data === data) {
+      return currentNode;
+    } else {
+      return this.findNodeRecursively(data, currentNode.next);
+    }
+  }
 
   printList() {
     let currentNode = this.head;

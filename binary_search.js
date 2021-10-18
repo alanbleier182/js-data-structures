a43bd7d6ee8178784b219069a72e238654f2dbc9
alanbleier182 <alanbleier182@gmail.com>
2021-10-18 15:39:05 -0500

    Create binary_search.js

---
+++ binary_search.js
@@ -0,0 +1,30 @@
+// This function returns the index of the target value from a sorted list
+// If the target value is not found, the function returns null
+const binarySearch = (arr, target) => {
+  let left = 0;
+  let right = arr.length;
+  
+  while (right > left) {
+    const indexToCheck = Math.floor((left + right) / 2);
+    const checking = arr[indexToCheck];
+    
+    if (checking === target) {
+      return indexToCheck;
+    } else if (checking < target) {
+      left = indexToCheck + 1;
+    } else {
+      right = indexToCheck;
+    }
+  }
+  
+  return null;
+}
+
+const searchable = [1, 2, 7, 8, 22, 28, 41, 58, 67, 71, 94];
+const target = 41;
+
+targetIndex = binarySearch(searchable, target);
+
+console.log(`The target index is ${targetIndex}.`);
+
+module.exports = binarySearch;

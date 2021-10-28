0ce7b1bf36d234748df225941198189187771063
alanbleier182 <alanbleier182@gmail.com>
2021-10-28 13:38:12 -0500

    Update breadthFirstTraversal.js

--- breadthFirstTraversal.js
+++ breadthFirstTraversal.js
@@ -1,14 +1,22 @@
 const testGraph = require('./testGraph.js');
+const Queue = require('./Queue.js');
 
 const breadthFirstTraversal = (start) => {
   const visitedVertices = [start];
-  start.edges.forEach( edge => {
-    const neighbor = edge.end;
-    if(!visitedVertices.includes(neighbor)) {
-      visitedVertices.push(neighbor);
+  const visitQueue = new Queue();
+  visitQueue.enqueue(start);
+  while (!visitQueue.isEmpty()) {
+    const current = visitQueue.dequeue();
+    console.log(current.data);
+    current.edges.forEach((edge) => {
+      const neighbor = edge.end;
+      if (!visitedVertices.includes(neighbor)) {
+        visitedVertices.push(neighbor);
+        visitQueue.enqueue(neighbor);
+      }
+    })
   }
-  });
-  console.log(visitedVertices);
+  
 };
 
 breadthFirstTraversal(testGraph.vertices[0]);

29a18ff1be0eea846f1784f41e9c802a685301e6
alanbleier182 <alanbleier182@gmail.com>
2021-10-28 13:13:54 -0500

    Update depthFirstTraversal.js

--- depthFirstTraversal.js
+++ depthFirstTraversal.js
@@ -3,16 +3,14 @@ const testGraph = require('./testGraph.js');
 const depthFirstTraversal = (start, visitedVertices = [start]) => {
   console.log(start.data);
 
-  if(start.edges.length) {
-    let neighbor = start.edges[0].end;
-    // console.log(neighbor.data);
-    if(!visitedVertices.includes(neighbor)) {
+  start.edges.forEach(edge => {
+    const neighbor = edge.end;
+
+    if (!visitedVertices.includes(neighbor)) {
       visitedVertices.push(neighbor);
       depthFirstTraversal(neighbor, visitedVertices);
-    }
-  }
-
-
-};
+      }
+    });
+  };
 
 depthFirstTraversal(testGraph.vertices[0]);

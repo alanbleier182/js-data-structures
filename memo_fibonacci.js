5ba38c111ad46b77824b69ca5be30598b49638b1
alanbleier182 <alanbleier182@gmail.com>
2021-11-08 11:13:37 -0600

    Create memo_fibonacci.js

---
+++ memo_fibonacci.js
@@ -0,0 +1,21 @@
+// A memoized fibonacci function to economize function calls
+
+const memo = {};
+
+const fibonacci = num => {
+  let returnValue;
+  
+  if (memo[num]) {
+    returnValue = memo[num];
+  } else if (num === 0 || num === 1) {
+    returnValue = num;
+  } else {
+    returnValue = fibonacci(num - 1) + fibonacci(num - 2);
+    memo[num] = returnValue;
+  }
+  return returnValue;
+}
+
+// Test
+console.log(fibonacci(20));
+console.log(fibonacci(200));
